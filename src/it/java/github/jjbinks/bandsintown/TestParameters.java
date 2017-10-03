5fc7da4d38f691b60f6173e388cf34940d28d83f
JayJayBings <profikerl@yahoo.de>
2017-10-03 14:25:02 +0200

    changed invalid artist parameter

--- src/it/java/github/jjbinks/bandsintown/TestParameters.java
+++ src/it/java/github/jjbinks/bandsintown/TestParameters.java
@@ -3,6 +3,6 @@ package github.jjbinks.bandsintown;
 public class TestParameters {
     public static final String VALID_APP_ID = "JJBinksIntegrationTest";
     public static final String INVALID_APP_ID = " ";
-    public static final String INVALID_ARTIST = "123321";
+    public static final String INVALID_ARTIST = "ThisIsAnInvalidArtist";
     public static final String VALID_ARTIST = "Grouplove";
 }
